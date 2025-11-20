0c4b1f88f6370b0a5f1d81b0cc334286ed50d753
lnkrr <lnkrr@proton.me>
2025-11-20 12:55:59 +0300

    feat: cross-compile

--- build.sh
+++ build.sh
@@ -2,12 +2,13 @@
 
 set -e
 
-if [ "$#" -ne 1 ]; then
-    echo "USAGE: ./build.sh <llvm-version>"
+if [ "$#" -ne 2 ]; then
+    echo "USAGE: ./build.sh <llvm version> <arch>"
     exit 1
 fi
 
 LLVM_VERSION="$1"
+LLVM_TARGET="$2-linux-gnu"
 
 rm -rf llvm-project install build
 
@@ -16,7 +17,26 @@ git clone https://github.com/llvm/llvm-project --depth 1 \
 
 mkdir install build && cd build
 
-cmake -G Ninja ../llvm-project/llvm -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_PREFIX=../install
+cat - <<EOF > $LLVM_TARGET.cmake
+    set(CMAKE_SYSTEM_NAME Linux)
+
+    set(CMAKE_C_COMPILER_TARGET $LLVM_TARGET)
+    set(CMAKE_CXX_COMPILER_TARGET $LLVM_TARGET)
+
+    set(CMAKE_LINKER_TYPE LLD)
+    set(CMAKE_C_COMPILER clang)
+    set(CMAKE_CXX_COMPILER clang++)
+
+    set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+    set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+    set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+    set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+EOF
+
+cmake -G Ninja ../llvm-project/llvm \
+    -DCMAKE_BUILD_TYPE=Debug \
+    -DCMAKE_TOOLCHAIN_FILE=$LLVM_TARGET.cmake \
+    -DLLVM_HOST_TRIPLE=$LLVM_TARGET \
+    -DCMAKE_INSTALL_PREFIX=../install/$LLVM_TARGET
 
 cmake --build . --target install
